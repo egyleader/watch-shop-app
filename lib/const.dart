59bbc5279cb059066272af0c4b1fde8ad58a669f
Ahmed Osama <ahmed.os.attya@gmail.com>
2020-02-18 18:47:58 +0200

    half first page

--- lib/const.dart
+++ lib/const.dart
@@ -2,18 +2,44 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-final ThemeData mainTheme = ThemeData.dark().copyWith(
+final ThemeData kMainTheme = ThemeData.dark().copyWith(
   accentColor: kAccentColor,
   cardColor: kAccentColor,
-  textTheme: TextTheme(title: TextStyle()),
+  textTheme: TextTheme(
+    body1: TextStyle(fontFamily: 'Bebas Neue', fontSize: 22, letterSpacing: 2),
+    body2: TextStyle(
+      fontFamily: 'Bebas Neue',
+      fontSize: 18,
+    ),
+    display1: TextStyle(
+      fontSize: 25.0,
+      letterSpacing: 2.0,
+      fontWeight: FontWeight.w900,
+      color: kBackgroundColor,
+    ),
+    display2: TextStyle(
+        fontSize: 16.0,
+        fontWeight: FontWeight.w600,
+        letterSpacing: 2.0,
+        color: kAccentDarkColor),
+        caption: TextStyle(fontSize: 14.0 ,fontWeight: FontWeight.w600, color: kAccentDarkColor ),
+    title:
+        TextStyle(fontWeight: FontWeight.w500, fontSize: 30, letterSpacing: 5),
+    subtitle: TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 18,
+      letterSpacing: 5,
+    ),
+    overline: TextStyle(fontSize: 14, letterSpacing: 4.0, color: kAccentColor),
+  ),
 );
 
-
-// color constants : change these colors to change all app look 
+// color constants : change these colors to change all app look
 
 const Color kBackgroundColor = Color(0xff233a66);
 const Color kAccentColor = Color(0xfffed691);
 const Color kTextColor = Color(0xffeeeddf);
+const Color kAccentDarkColor = Color(0xffa37e56);
 
 const Gradient kBackgroundGradient = RadialGradient(
   center: Alignment(0.7, -0.4), // near the top right
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:watch_shop_app/const.dart';
 
 void main() => runApp(MyApp());
@@ -8,9 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: ' Watch Shop app ',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: kMainTheme,
       home: MyHomePage(),
     );
   }
@@ -19,11 +19,114 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Some repetitive values for shorting the code
+
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: kBackgroundGradient
+      body: SafeArea(
+        child: Container(
+          height: height,
+          width: width,
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(gradient: kBackgroundGradient),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(
+                        Ionicons.ios_menu,
+                        size: 35.0,
+                        color: kAccentColor,
+                      ),
+                      onPressed: () {}),
+                  Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Ionicons.ios_search,
+                            size: 35.0, color: kTextColor),
+                      ),
+                      IconButton(
+                        icon: Icon(Ionicons.ios_cart,
+                            size: 35.0, color: kTextColor),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: height / 10),
+              Text(
+                'FEATURED',
+                style: textTheme.subtitle,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('PRODUCTS', style: textTheme.title),
+                  Row(
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(
+                            Ionicons.ios_arrow_back,
+                            size: 35.0,
+                            color: kAccentColor,
+                          ),
+                          onPressed: () {}),
+                      IconButton(
+                          icon: Icon(
+                            Ionicons.ios_arrow_forward,
+                            size: 35.0,
+                            color: kAccentColor,
+                          ),
+                          onPressed: () {}),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 15.0),
+                height: 200.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/watch01.png'),
+                    fit: BoxFit.contain,
+                    alignment: Alignment.topRight,
+                  ),
+                  borderRadius: BorderRadius.circular(25.0),
+                  color: kAccentColor,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'WSD-006',
+                      style: textTheme.display2,
+                    ),
+                    Text(
+                      'WISHDOIT',
+                      style: textTheme.display1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical:10.0),
+                      child: SizedBox(
+                        width: width/2.8,
+                                              child: Text(
+                            'Fashion Men Quartz Watch Luxury Leather Strap Waterproof' , style: textTheme.caption,),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
